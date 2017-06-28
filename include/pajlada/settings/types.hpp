7b1b084af768c7fe076f520e2329019b26928598
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2017-06-28 14:40:39 +0200

    Create two dummy ==operators for Object/Array

--- include/pajlada/settings/types.hpp
+++ include/pajlada/settings/types.hpp
@@ -5,10 +5,18 @@ namespace Settings {
 
 // Custom "JSON Object {}" type
 struct Object {
+    bool operator==(const Object &rhs)
+    {
+        return false;
+    }
 };
 
 // Custom "JSON Array []" type
 struct Array {
+    bool operator==(const Array &rhs)
+    {
+        return false;
+    }
 };
 
 }  // namespace Settings
