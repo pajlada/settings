8309a17041777dde281da617a5e4460df42ba5b9
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-06-26 00:37:41 +0000

    Improve tests

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "pajlada/settings/settingdata.hpp"
 #include "pajlada/settings/settingmanager.hpp"
 
+#define CATCH_CONFIG_MAIN
 #include "libs/catch.hpp"
 
 #include <cassert>
@@ -709,6 +710,14 @@ TEST_CASE("Channel", "[settings]")
     REQUIRE(chPajlada.maxMessageLength == 500);
 }
 
+#ifdef PAJLADA_SETTINGS_ENABLE_EXCEPTIONS
+#define DD_THROWS(x) REQUIRE_THROWS(x)
+#define REQUIRE_IF_NOEXCEPT(x, y)
+#else
+#define DD_THROWS(x) x
+#define REQUIRE_IF_NOEXCEPT(x, y) REQUIRE(x->getValue() == y)
+#endif
+
 TEST_CASE("Load files", "[settings]")
 {
     SECTION("Invalid files")
@@ -822,6 +831,86 @@ TEST_CASE("Simple static", "[settings]")
     Foo::f7 = 0.f;
 
     REQUIRE(SettingManager::saveAs("files/out.test2.json") == true);
+}
+
+TEST_CASE("Deserialize", "[settings]")
+{
+    SettingManager::clear();
+    REQUIRE(LoadFile("deserialize-samples.json"));
+
+    SECTION("int")
+    {
+        Setting<int> *a3, *a4;
+        Setting<int> a1("/int");
+        Setting<int> a2("/float");
+        DD_THROWS(a3 = new Setting<int>("/string"));
+        DD_THROWS(a4 = new Setting<int>("/bool"));
+
+        REQUIRE(a1.getValue() == 5);
+        REQUIRE(a2.getValue() == 5);
+        REQUIRE_IF_NOEXCEPT(a3, 0);
+        REQUIRE_IF_NOEXCEPT(a4, 0);
+    }
+
+    SECTION("float")
+    {
+        Setting<float> *a3, *a4;
+        Setting<float> a1("/int");
+        Setting<float> a2("/float");
+        DD_THROWS(a3 = new Setting<float>("/string"));
+        DD_THROWS(a4 = new Setting<float>("/bool"));
+
+        REQUIRE(a1.getValue() == Approx(5.f));
+        REQUIRE(a2.getValue() == Approx(5.3f));
+        REQUIRE_IF_NOEXCEPT(a3, 0);
+        REQUIRE_IF_NOEXCEPT(a4, 0);
+    }
+
+    SECTION("double")
+    {
+        Setting<double> *a3 = nullptr;
+        Setting<double> *a4 = nullptr;
+        Setting<double> a1("/int");
+        Setting<double> a2("/float");
+        DD_THROWS(a3 = new Setting<double>("/string"));
+        DD_THROWS(a4 = new Setting<double>("/bool"));
+
+        REQUIRE(a1.getValue() == Approx(5.));
+        REQUIRE(a2.getValue() == Approx(5.3));
+        REQUIRE_IF_NOEXCEPT(a3, 0);
+        REQUIRE_IF_NOEXCEPT(a4, 0);
+    }
+
+    SECTION("bool")
+    {
+        Setting<bool> *a2, *a3, *a4;
+        Setting<bool> a1("/int");
+        DD_THROWS(a2 = new Setting<bool>("/float"));
+        DD_THROWS(a3 = new Setting<bool>("/string"));
+        a4 = new Setting<bool>("/bool");
+
+        Setting<bool> a5("/int1");
+
+        REQUIRE(a1.getValue() == false);
+        REQUIRE_IF_NOEXCEPT(a2, false);
+        REQUIRE_IF_NOEXCEPT(a3, false);
+        REQUIRE(a4->getValue() == true);
+        REQUIRE(a5.getValue() == true);
+    }
+
+    SECTION("string")
+    {
+        Setting<std::string> *a1, *a2, *a3, *a4;
+        DD_THROWS(a1 = new Setting<std::string>("/int"));
+        DD_THROWS(a2 = new Setting<std::string>("/float"));
+        a3 = new Setting<std::string>("/string");
+        DD_THROWS(a4 = new Setting<std::string>("/bool"));
+
+        REQUIRE_IF_NOEXCEPT(a1, "");
+        REQUIRE_IF_NOEXCEPT(a2, "");
+        REQUIRE(a3->getValue() == "xd");
+        REQUIRE_IF_NOEXCEPT(a4, "");
+    }
 }
 
 // NOTE: This must be at the bottom to not disrupt with other
