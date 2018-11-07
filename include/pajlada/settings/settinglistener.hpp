d1b983139cd0131a3c966f2480d8f235e5e3bc07
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-11-07 11:38:13 +0100

    Make setting listener more safe, and let it live in just the pajlada namespace

--- include/pajlada/settings/settinglistener.hpp
+++ include/pajlada/settings/settinglistener.hpp
@@ -1,51 +1,58 @@
 #pragma once
 
-#include <pajlada/settings/setting.hpp>
-#include <pajlada/settings/settingdata.hpp>
-
 #include <pajlada/signals/connection.hpp>
 
 namespace pajlada {
-namespace Settings {
+
+namespace detail {
+
+struct CallbackCaller {
+    std::function<void()> cb;
+
+    void
+    CallCallback()
+    {
+        this->cb();
+    }
+};
+
+}  // namespace detail
 
 class SettingListener
 {
 public:
-    using Callback = std::function<void(const SignalArgs &)>;
+    using Callback = std::function<void()>;
 
-    SettingListener() = default;
-
-    SettingListener(Callback _cb)
-        : cb(std::move(_cb))
+    SettingListener()
     {
+        this->callbackCaller = std::make_unique<detail::CallbackCaller>();
+    }
+
+    SettingListener(Callback cb)
+    {
+        this->callbackCaller = std::make_unique<detail::CallbackCaller>();
+
+        this->callbackCaller->cb = cb;
+    }
+
+    void
+    setCB(Callback cb)
+    {
+        this->callbackCaller->cb = cb;
     }
 
     SettingListener(SettingListener &&other) noexcept
-        : cb(std::move(other.cb))
-        , settings(std::move(other.settings))
     {
-        other.managedConnections.clear();
+        this->callbackCaller = std::move(other.callbackCaller);
 
-        for (auto &setting : this->settings) {
-            if (auto s = setting.lock()) {
-                this->addRawSetting(s);
-            }
-        }
+        this->managedConnections.swap(other.managedConnections);
     }
 
     SettingListener &
     operator=(SettingListener &&other) noexcept
     {
-        other.managedConnections.clear();
-
-        this->cb = std::move(other.cb);
-        this->settings = std::move(other.settings);
-
-        for (auto &setting : this->settings) {
-            if (auto s = setting.lock()) {
-                this->addRawSetting(s);
-            }
-        }
+        this->callbackCaller = std::move(other.callbackCaller);
+        this->managedConnections.swap(other.managedConnections);
 
         return *this;
     }
@@ -53,41 +60,19 @@ public:
     SettingListener(const SettingListener &) = delete;
     SettingListener &operator=(const SettingListener &) = delete;
 
-    void
-    addRawSetting(std::shared_ptr<SettingData> setting)
-    {
-        auto connection = setting->updated.connect(
-            [this](const rapidjson::Value &, const SignalArgs &args) {
-                if (this->cb) {
-                    this->cb(args);
-                }
-            });
-
-        this->managedConnections.emplace_back(std::move(connection));
-    }
-
     template <typename AnySetting>
     void
     addSetting(AnySetting &setting, bool autoInvoke = false)
     {
-        this->settings.emplace_back(setting.getData());
-
-        setting.connectSimple(
-            [this](const SignalArgs &args) {
-                if (this->cb) {
-                    this->cb(args);
-                }
-            },
-            this->managedConnections, autoInvoke);
+        setting.connectSimple(std::bind(&detail::CallbackCaller::CallCallback,
+                                        this->callbackCaller.get()),
+                              this->managedConnections, autoInvoke);
     }
 
-    Callback cb;
-
 private:
-    std::vector<std::weak_ptr<SettingData>> settings;
-
     std::vector<Signals::ScopedConnection> managedConnections;
+
+    std::unique_ptr<detail::CallbackCaller> callbackCaller{};
 };
 
-}  // namespace Settings
 }  // namespace pajlada
