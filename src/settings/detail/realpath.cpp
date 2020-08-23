032453876ed1dbed30e54b3d7a595090f3971760
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2020-08-23 15:13:23 +0200

    Remove debug output

--- src/settings/detail/realpath.cpp
+++ src/settings/detail/realpath.cpp
@@ -25,8 +25,6 @@ RealPath(const fs::path &_path, fs_error_code &ec)
 
     const auto relativePath = path.parent_path();
 
-    PS_DEBUG("Parent path: " << relativePath);
-
     std::unordered_set<fs::path::string_type> seenPaths;
 
     do {
@@ -42,17 +40,13 @@ RealPath(const fs::path &_path, fs_error_code &ec)
         if (!symlinkResponse.is_absolute()) {
             path = relativePath / symlinkResponse;
         } else {
-            PS_DEBUG("Is absolute xd: " << symlinkResponse);
             path = symlinkResponse;
         }
         if (ec) {
-            PS_DEBUG("EC: " << ec);
             return path;
         }
         isSymlink = fs::is_symlink(path, ec);
         if (ec) {
-            PS_DEBUG("not a symlink: " << ec);
-            PS_DEBUG("Final path2: " << path);
             // Not an error - the symlink might have just stopped here at a file that doesn't exist (yet)
             ec = {};
             return path;
@@ -60,7 +54,6 @@ RealPath(const fs::path &_path, fs_error_code &ec)
 
     } while (isSymlink);
 
-    PS_DEBUG("Final path: " << path);
     return path;
 }
 
--- src/settings/settingmanager.cpp
+++ src/settings/settingmanager.cpp
@@ -369,9 +369,6 @@ SettingManager::loadFrom(const fs::path &_path)
 
     auto path = detail::RealPath(_path, ec);
 
-    PS_DEBUG("Original path: " << _path);
-    PS_DEBUG("Real path: " << path);
-
     if (ec) {
         return LoadError::FileHandleError;
     }
