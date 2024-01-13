b451162483a5d8436587e2b04d0afee54c15b48a
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-01-13 12:06:45 +0100

    Fix boost::any tests

--- src/test/map.cpp
+++ src/test/map.cpp
@@ -5,62 +5,64 @@
 using namespace pajlada::Settings;
 
 #ifdef PAJLADA_BOOST_ANY_SUPPORT
-TEST_CASE("Simple Map", "[settings]")
+TEST(Map, Simple)
 {
     using boost::any_cast;
 
-    Setting<map<string, boost::any>> test("/map");
+    Setting<std::map<std::string, boost::any>> test("/map");
 
-    REQUIRE(LoadFile("in.simplemap.json"));
+    EXPECT_TRUE(LoadFile("in.simplemap.json"));
 
     auto myMap = test.getValue();
-    REQUIRE(myMap.size() == 3);
-    REQUIRE(any_cast<int>(myMap["a"]) == 1);
-    REQUIRE(any_cast<string>(myMap["b"]) == "asd");
-    REQUIRE(any_cast<double>(myMap["c"]) == 3.14);
+    EXPECT_TRUE(myMap.size() == 3);
+    EXPECT_TRUE(any_cast<int>(myMap["a"]) == 1);
+    EXPECT_TRUE(any_cast<std::string>(myMap["b"]) == "asd");
+    EXPECT_TRUE(any_cast<double>(myMap["c"]) == 3.14);
 
-    vector<string> keys{"a", "b", "c"};
+    std::vector<std::string> keys{"a", "b", "c"};
 
-    REQUIRE(keys == SettingManager::getObjectKeys("/map"));
+    EXPECT_TRUE(keys == SettingManager::getObjectKeys("/map"));
 
-    REQUIRE(SettingManager::gSaveAs("files/out.simplemap.json"));
+    EXPECT_TRUE(SettingManager::gSaveAs("files/out.simplemap.json"));
 }
 
-TEST_CASE("Complex Map", "[settings]")
+TEST(Map, Complex)
 {
     using boost::any_cast;
 
-    Setting<map<string, boost::any>> test("/map");
+    Setting<std::map<std::string, boost::any>> test("/map");
 
-    REQUIRE(LoadFile("in.complexmap.json"));
+    EXPECT_TRUE(LoadFile("in.complexmap.json"));
 
     auto myMap = test.getValue();
-    REQUIRE(myMap.size() == 3);
-    REQUIRE(any_cast<int>(myMap["a"]) == 5);
+    EXPECT_TRUE(myMap.size() == 3);
+    EXPECT_TRUE(any_cast<int>(myMap["a"]) == 5);
 
-    auto innerMap = any_cast<map<string, boost::any>>(myMap["innerMap"]);
-    REQUIRE(innerMap.size() == 3);
-    REQUIRE(any_cast<int>(innerMap["a"]) == 420);
-    REQUIRE(any_cast<int>(innerMap["b"]) == 320);
-    REQUIRE(any_cast<double>(innerMap["c"]) == 13.37);
+    auto innerMap =
+        any_cast<std::map<std::string, boost::any>>(myMap["innerMap"]);
+    EXPECT_TRUE(innerMap.size() == 3);
+    EXPECT_TRUE(any_cast<int>(innerMap["a"]) == 420);
+    EXPECT_TRUE(any_cast<int>(innerMap["b"]) == 320);
+    EXPECT_TRUE(any_cast<double>(innerMap["c"]) == 13.37);
 
-    auto innerArray = any_cast<vector<boost::any>>(myMap["innerArray"]);
-    REQUIRE(innerArray.size() == 9);
-    REQUIRE(any_cast<int>(innerArray[0]) == 1);
-    REQUIRE(any_cast<int>(innerArray[1]) == 2);
-    REQUIRE(any_cast<int>(innerArray[2]) == 3);
-    REQUIRE(any_cast<int>(innerArray[3]) == 4);
-    REQUIRE(any_cast<string>(innerArray[4]) == "testman");
-    REQUIRE(any_cast<bool>(innerArray[5]) == true);
-    REQUIRE(any_cast<bool>(innerArray[6]) == false);
-    REQUIRE(any_cast<double>(innerArray[7]) == 4.20);
+    auto innerArray = any_cast<std::vector<boost::any>>(myMap["innerArray"]);
+    EXPECT_TRUE(innerArray.size() == 9);
+    EXPECT_TRUE(any_cast<int>(innerArray[0]) == 1);
+    EXPECT_TRUE(any_cast<int>(innerArray[1]) == 2);
+    EXPECT_TRUE(any_cast<int>(innerArray[2]) == 3);
+    EXPECT_TRUE(any_cast<int>(innerArray[3]) == 4);
+    EXPECT_TRUE(any_cast<std::string>(innerArray[4]) == "testman");
+    EXPECT_TRUE(any_cast<bool>(innerArray[5]) == true);
+    EXPECT_TRUE(any_cast<bool>(innerArray[6]) == false);
+    EXPECT_TRUE(any_cast<double>(innerArray[7]) == 4.20);
 
-    auto innerArrayMap = any_cast<map<string, boost::any>>(innerArray[8]);
-    REQUIRE(innerArrayMap.size() == 3);
-    REQUIRE(any_cast<int>(innerArrayMap["a"]) == 1);
-    REQUIRE(any_cast<int>(innerArrayMap["b"]) == 2);
-    REQUIRE(any_cast<int>(innerArrayMap["c"]) == 3);
+    auto innerArrayMap =
+        any_cast<std::map<std::string, boost::any>>(innerArray[8]);
+    EXPECT_TRUE(innerArrayMap.size() == 3);
+    EXPECT_TRUE(any_cast<int>(innerArrayMap["a"]) == 1);
+    EXPECT_TRUE(any_cast<int>(innerArrayMap["b"]) == 2);
+    EXPECT_TRUE(any_cast<int>(innerArrayMap["c"]) == 3);
 
-    REQUIRE(SettingManager::gSaveAs("files/out.complexmap.json"));
+    EXPECT_TRUE(SettingManager::gSaveAs("files/out.complexmap.json"));
 }
 #endif
