e3abc1b442bc20473736a7ba6481da20f56e3083
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-01-10 16:02:16 +0100

    refactor: misc tests

--- tests/src/channel.cpp
+++ tests/src/channel.cpp
@@ -6,17 +6,19 @@ namespace pajlada::test {
 
 using namespace Settings;
 
-IndexedChannel::IndexedChannel(const unsigned _index)
+IndexedChannel::IndexedChannel(const unsigned _index,
+                               const std::shared_ptr<SettingManager> &sm)
     : index(_index)
-    , xDIndex("/channelsArray/" + std::to_string(index) + "/index", _index)
+    , xDIndex("/channelsArray/" + std::to_string(index) + "/index", _index, sm)
     , name("/channelsArray/" + std::to_string(index) + "/name",
-           "Name not loaded")
+           "Name not loaded", sm)
 {
 }
 
-Channel::Channel(const std::string &_name)
+Channel::Channel(const std::string &_name,
+                 const std::shared_ptr<SettingManager> &sm)
     : name(_name)
-    , maxMessageLength("/channels/" + this->name + "/maxMessageLength", 240)
+    , maxMessageLength("/channels/" + this->name + "/maxMessageLength", 240, sm)
 {
 }
 
