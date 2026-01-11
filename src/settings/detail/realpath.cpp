ebb8e3abd0b92a90edbcbeb7ceb57ad8e2bc72b1
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-01-11 19:43:35 +0100

    refactor realpath

--- src/settings/detail/realpath.cpp
+++ src/settings/detail/realpath.cpp
@@ -1,19 +1,20 @@
 #include <pajlada/settings/detail/realpath.hpp>
-#include <pajlada/settings/internal.hpp>
-#include <unordered_set>
 
 namespace pajlada::Settings::detail {
 
 std::filesystem::path
 RealPath(const std::filesystem::path &_path, std::error_code &ec)
 {
-    std::filesystem::path path{_path};
+    auto path = std::filesystem::weakly_canonical(_path, ec);
+    if (ec) {
+        return _path;
+    }
 
     auto isSymlink = std::filesystem::is_symlink(path, ec);
 
     if (ec) {
         // Not an error - the symlink might have just stopped here at a file that doesn't exist (yet)
-        ec = {};
+        ec.clear();
         return path;
     }
 
@@ -21,37 +22,22 @@ RealPath(const std::filesystem::path &_path, std::error_code &ec)
         return path;
     }
 
-    const auto relativePath = path.parent_path();
+    auto symlinkTarget = std::filesystem::read_symlink(path, ec);
+    if (ec) {
+        // Something went wrong reading the symlink, return original canonical path
+        return path;
+    }
+    if (symlinkTarget.is_absolute()) {
+        return symlinkTarget;
+    }
 
-    std::unordered_set<std::filesystem::path::string_type> seenPaths;
-
-    do {
-        auto pathString = path.native();
-        if (seenPaths.count(pathString) != 0) {
-            ec = std::make_error_code(std::errc::too_many_symbolic_link_levels);
-            return path;
-        }
-
-        seenPaths.emplace(pathString);
-        auto symlinkResponse = std::filesystem::read_symlink(path, ec);
-        if (!symlinkResponse.is_absolute()) {
-            path = relativePath / symlinkResponse;
-        } else {
-            path = symlinkResponse;
-        }
-        if (ec) {
-            return path;
-        }
-        isSymlink = std::filesystem::is_symlink(path, ec);
-        if (ec) {
-            // Not an error - the symlink might have just stopped here at a file that doesn't exist (yet)
-            ec = {};
-            return path;
-        }
-
-    } while (isSymlink);
-
-    return path;
+    auto absolutePath =
+        std::filesystem::absolute(path.parent_path() / symlinkTarget, ec);
+    if (ec) {
+        // Something went wrong making the final symlink target absolute, return original canonical path
+        return path;
+    }
+    return absolutePath;
 }
 
 }  // namespace pajlada::Settings::detail
